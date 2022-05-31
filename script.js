1986c1a1411fdb06dc4133205d87272fe19c6367
terjeee <96795863+terjeee@users.noreply.github.com>
2022-05-31 05:11:29 +0200

    ?

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const adviceNumber = document.querySelector('#advice--number');
 const adviceContent = document.querySelector(
   '.advice__contenct'
 );
+
+async function getAdvice() {
+  const advice = await fetch(
+    'https://api.adviceslip.com/advice'
+  ).then(response => response.json());
+
+  console.log(advice.slip);
+}
+
+getAdvice();
